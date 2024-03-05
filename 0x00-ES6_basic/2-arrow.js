c15d8fe73d7e96d9945810e95a1c5e0f57a4a4c4
System Administrator <root@Hassanyoung1.local>
2024-03-06 09:59:55 +1000

    writing arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,12 @@
 /*
-*Rewrite the following standard function to use ES6’s arrow syntax of the function add
+*Rewrite the following standard function to use
+*ES6’s arrow syntax of the function add
 */
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  self.addNeighborhood = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
