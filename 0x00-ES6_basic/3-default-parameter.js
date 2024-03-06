cb60ea0061c791e13b57418a225d6c06154f3283
System Administrator <root@Hassanyoung1.local>
2024-03-06 10:52:36 +1000

    writing arrow function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,14 +1,7 @@
 /*
-*Condense the internals function to 1 line - without changing the name of each function/variable.
+*Condense the internals function to 1 line - without 
+*changing the name of each function/variable.
 */
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-/* if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
-*/
   return initialNumber + expansion1989 + expansion2019;
 }
