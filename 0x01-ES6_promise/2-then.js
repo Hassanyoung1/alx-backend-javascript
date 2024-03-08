887a12d9edd13dd062dc58c1b4cef1b0a7737743
MacBook <macbook@Hassanyoung1.local>
2024-03-09 00:37:51 +1000

    Append three handlers to the function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+/*
+*Append three handlers to the function
+*/
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error('An error occurred'))
+    .finally(() => console.log('Got a response from the API'));
+}
