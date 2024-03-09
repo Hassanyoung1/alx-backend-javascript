24464827712a3f3a6fffcd2d1aa1d42e3966d3b6
MacBook <macbook@Hassanyoung1.local>
2024-03-09 09:57:37 +0100

    Simple promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils'; // Removed ".js" extension
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName, lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
