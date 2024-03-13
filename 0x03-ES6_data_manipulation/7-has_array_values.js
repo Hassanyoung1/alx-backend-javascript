b476abc39e2adb03ccc988ab173a39e15bb88eb7
MacBook <macbook@Hassanyoung1.local>
2024-03-13 15:20:58 +0100

    a function named hasValuesFromArray that returns a boolean if all the elements in the array exist within the set.

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,7 @@
 /*
-*a function named hasValuesFromArray that returns 
-*a boolean if all the elements in the array exist within the set*/
+* a function named hasValuesFromArray that returns
+* a boolean if all the elements in the array exist within the set
+*/
 export default function hasValuesFromArray(set, array) {
   return array.every((element) => set.has(element));
 }
