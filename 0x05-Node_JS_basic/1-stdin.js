ff07d3a319f743a9ac5f3ca6e9cfd7f940b5da3c
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-05-01 14:19:43 +0100

    Using

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+
+process.stdin.on('data', (chunk) => {
+  const input = chunk.toString().trim(); // Remove whitespace and newline characters
+  if (input) {
+    process.stdout.write(`Your name is: ${input}\n`);
   }
 });
+
 process.stdin.on('end', () => {
-  process.stdout.write(' This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
