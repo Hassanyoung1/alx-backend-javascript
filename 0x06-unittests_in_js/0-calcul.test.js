89abdb5d4e6eced4ae7e6cf9a7d5e582abd72a3f
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-05-03 11:02:42 +0100

    Basic

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,10 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', function() {
-    it("checks assert", function() {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1, 3.7), 5);
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+    it("checks output", function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
