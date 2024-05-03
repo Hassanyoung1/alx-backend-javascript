ab67bd61403d1e4d3639b8ed189a01bd061283f4
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-05-03 23:02:03 +0100

    basic

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const { app } = require('./api');
+
+describe('API', () => {
+    it('should return 200', async () => {
+        const response = await app.inject({
+        method: 'GET',
+        url: '/',
+        });
+
+        assert.strictEqual(response.statusCode, 200);
+    });
+    });
